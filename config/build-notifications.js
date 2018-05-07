0dc8023b7efe538eea62e6da3c6594aabd678a73
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-05-07 19:50:08 +0100

    Add ember-cli-build-notifications

---
+++ config/build-notifications.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+    buildError: {
+        notify: true,
+        notificationOptions: {
+            sound: true
+        }
+    },
+    buildSuccess: {
+        notify: true,
+        notificationOptions: {
+            sound: true
+        }
+    }
+};
